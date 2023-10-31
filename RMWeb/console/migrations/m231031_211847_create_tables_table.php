c5bcd1b151850638d9337e3ffad79346907ee276
Marcelo Marques <2200428@ipleiria.pt>
2023-10-31 21:23:20 +0000

    Migração - Mesas

---
+++ RMWeb/console/migrations/m231031_211847_create_tables_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%tables}}`.
+ * Has foreign keys to the tables:
+ *
+ * - `{{%zones}}`
+ */
+class m231031_211847_create_tables_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%tables}}', [
+            'id' => $this->primaryKey(),
+            'zone_id' => $this->integer()->notNull(),
+            'description' => $this->string(200),
+        ]);
+
+        // creates index for column `zone_id`
+        $this->createIndex(
+            '{{%idx-tables-zone_id}}',
+            '{{%tables}}',
+            'zone_id'
+        );
+
+        // add foreign key for table `{{%zones}}`
+        $this->addForeignKey(
+            '{{%fk-tables-zone_id}}',
+            '{{%tables}}',
+            'zone_id',
+            '{{%zones}}',
+            'id',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        // drops foreign key for table `{{%zones}}`
+        $this->dropForeignKey(
+            '{{%fk-tables-zone_id}}',
+            '{{%tables}}'
+        );
+
+        // drops index for column `zone_id`
+        $this->dropIndex(
+            '{{%idx-tables-zone_id}}',
+            '{{%tables}}'
+        );
+
+        $this->dropTable('{{%tables}}');
+    }
+}
