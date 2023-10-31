84725bed658bf66c6af154b2e70f3af33641d3cb
joaomonteiro7 <2220892@my.ipleiria.pt>
2023-10-31 22:53:07 +0000

    Rbac modifications

--- RMWeb/console/migrations/m130524_201442_init.php
+++ RMWeb/console/migrations/m130524_201442_init.php
@@ -6,54 +6,28 @@ class m130524_201442_init extends Migration
 {
     public function up()
     {
-        $auth = Yii::$app->authManager;
-        #region roles
-        //Create roles
-        $role_admin = $auth->createRole('admin');
-        $auth->add($role_admin);
-        $role_employee = $auth->createRole('employee');
-        $auth->add($role_employee);
-        $role_chef = $auth->createRole('chef');
-        $auth->add($role_chef);
-        $role_client = $auth->createRole('client');
-        $auth->add($role_client);
-        $role_manager = $auth->createRole('manager');
-        $auth->add($role_manager);
-        #endregion
-        #region permissions
-        // permissions
-        $permRestaurantManagement = $auth->createPermission('RestaurantManagement'); //perm admin
-        $permStockManagement = $auth->createPermission('StockManagement'); //perm chef
-        $permOrderManagement = $auth->createPermission('OrderManagement'); //perm employee
-        $permUserManagement = $auth->createPermission('UserManagement'); // perm manager
-        $permReservationManagement = $auth->createPermission('ReservationManagement'); // perm client
-        $permTablesZones = $auth->createPermission('TablesZones'); // perm manager
-        #endregion        
-        #region child role->perm
-        // add childs role --> permission
-        $auth->addChild($role_admin, $permRestaurantManagement);
-        $auth->addChild($role_chef, $permStockManagement);
-        $auth->addChild($role_employee, $permOrderManagement);
-        $auth->addChild($role_manager, $permUserManagement);
-        $auth->addChild($role_client, $permReservationManagement);
-        $auth->addChild($role_manager, $permTablesZones);
-        #endregion       
-        #region inherits
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // https://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
 
-        // add childs inherits role --> role
-        $auth->addChild($role_admin, $role_manager);
-        $auth->addChild($role_manager, $role_chef);
-        $auth->addChild($role_manager, $role_employee);
-        $auth->addChild($role_employee, $role_client);
-        #endregion
-        // first user get id 1
-        $auth->assign($role_admin, 1);
-    }   
+        $this->createTable('{{%user}}', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string()->notNull()->unique(),
+            'auth_key' => $this->string(32)->notNull(),
+            'password_hash' => $this->string()->notNull(),
+            'password_reset_token' => $this->string()->unique(),
+            'email' => $this->string()->notNull()->unique(),
+
+            'status' => $this->smallInteger()->notNull()->defaultValue(10),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ], $tableOptions);
+    }
 
     public function down()
     {
-        $auth = Yii::$app->authManager;
-
-        $auth->removeAll();
+        $this->dropTable('{{%user}}');
     }
 }
--- RMWeb/console/migrations/m231031_204450_init_rbac.php
+++ RMWeb/console/migrations/m231031_204450_init_rbac.php
@@ -12,7 +12,48 @@ class m231031_204450_init_rbac extends Migration
      */
     public function safeUp()
     {
-
+        $auth = Yii::$app->authManager;
+        #region roles
+        //Create roles
+        $role_admin = $auth->createRole('admin');
+        $auth->add($role_admin);
+        $role_employee = $auth->createRole('employee');
+        $auth->add($role_employee);
+        $role_chef = $auth->createRole('chef');
+        $auth->add($role_chef);
+        $role_client = $auth->createRole('client');
+        $auth->add($role_client);
+        $role_manager = $auth->createRole('manager');
+        $auth->add($role_manager);
+        #endregion
+        #region permissions
+        // permissions
+        $permRestaurantManagement = $auth->createPermission('RestaurantManagement'); //perm admin
+        $permStockManagement = $auth->createPermission('StockManagement'); //perm chef
+        $permOrderManagement = $auth->createPermission('OrderManagement'); //perm employee
+        $permUserManagement = $auth->createPermission('UserManagement'); // perm manager
+        $permReservationManagement = $auth->createPermission('ReservationManagement'); // perm client
+        $permTablesZones = $auth->createPermission('TablesZones'); // perm manager
+        #endregion        
+        #region child role->perm
+        // add childs role --> permission
+        $auth->addChild($role_admin, $permRestaurantManagement);
+        $auth->addChild($role_chef, $permStockManagement);
+        $auth->addChild($role_employee, $permOrderManagement);
+        $auth->addChild($role_manager, $permUserManagement);
+        $auth->addChild($role_client, $permReservationManagement);
+        $auth->addChild($role_manager, $permTablesZones);
+        #endregion       
+        #region inherits
+    
+        // add childs inherits role --> role
+        $auth->addChild($role_admin, $role_manager);
+        $auth->addChild($role_manager, $role_chef);
+        $auth->addChild($role_manager, $role_employee);
+        $auth->addChild($role_employee, $role_client);
+        #endregion
+        // first user get id 1
+        $auth->assign($role_admin, 1);
     }
 
     /**
@@ -20,9 +61,8 @@ class m231031_204450_init_rbac extends Migration
      */
     public function safeDown()
     {
-        echo "m231031_204450_init_rbac cannot be reverted.\n";
-
-        return false;
+        $auth = Yii::$app->authManager;
+        $auth->removeAll();
     }
 
     /*
