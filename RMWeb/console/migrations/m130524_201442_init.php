4c136d9f21eb49a518c2cf5e601f61f7ab6b7c0e
joaomonteiro7 <2220892@my.ipleiria.pt>
2023-10-31 22:03:33 +0000

    Rbac update

--- RMWeb/console/migrations/m130524_201442_init.php
+++ RMWeb/console/migrations/m130524_201442_init.php
@@ -7,7 +7,7 @@ class m130524_201442_init extends Migration
     public function up()
     {
         $auth = Yii::$app->authManager;
-
+        #region roles
         //Create roles
         $role_admin = $auth->createRole('admin');
         $auth->add($role_admin);
@@ -19,22 +19,36 @@ class m130524_201442_init extends Migration
         $auth->add($role_client);
         $role_manager = $auth->createRole('manager');
         $auth->add($role_manager);
+        #endregion
+        #region permissions
+        // permissions
+        $permRestaurantManagement = $auth->createPermission('RestaurantManagement'); //perm admin
+        $permStockManagement = $auth->createPermission('StockManagement'); //perm chef
+        $permOrderManagement = $auth->createPermission('OrderManagement'); //perm employee
+        $permUserManagement = $auth->createPermission('UserManagement'); // perm manager
+        $permReservationManagement = $auth->createPermission('ReservationManagement'); // perm client
+        $permTablesZones = $auth->createPermission('TablesZones'); // perm manager
+        #endregion        
+        #region child role->perm
+        // add childs role --> permission
+        $auth->addChild($role_admin, $permRestaurantManagement);
+        $auth->addChild($role_chef, $permStockManagement);
+        $auth->addChild($role_employee, $permOrderManagement);
+        $auth->addChild($role_manager, $permUserManagement);
+        $auth->addChild($role_client, $permReservationManagement);
+        $auth->addChild($role_manager, $permTablesZones);
+        #endregion       
+        #region inherits
 
-       // $auth->addChild();
-       
-        $permRestaurantManagement = $auth->createPermission('RestaurantManagement');
-        $permStockManagement = $auth->createPermission('StockManagement'); //duvida
-        $permOrderManagement = $auth->createPermission('OrderManagement');
-        $permUserManagement = $auth->createPermission('UserManagement');
-        $permReservationManagement = $auth->createPermission('ReservationManagement');
-
-
-
+        // add childs inherits role --> role
+        $auth->addChild($role_admin, $role_manager);
+        $auth->addChild($role_manager, $role_chef);
+        $auth->addChild($role_manager, $role_employee);
+        $auth->addChild($role_employee, $role_client);
+        #endregion
+        // first user get id 1
         $auth->assign($role_admin, 1);
-        
-        
-        //create permitions
-    }
+    }   
 
     public function down()
     {
