8b007bef7b52dbe0ff161bf9246511d702ea8d53
DavidDomingues <2220897@my.ipleiria.pt>
2023-11-24 18:12:29 +0000

    Ultimas alterações
    Finalizei o template de frontoffice

--- RMWeb/frontend/views/layouts/main.php
+++ RMWeb/frontend/views/layouts/main.php
@@ -64,7 +64,7 @@ AppAsset::register($this);
 
         <nav class="navbar navbar-expand-lg navbar-light py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
             <a href="index.php" class="navbar-brand ms-4 ms-lg-0">
-                <img src="assets/img/png/logo-no-background.png" width="125" height="42,8">
+                <img src="assets/img/png/logo-no-background.png" width="180" height="61,74">
             </a>
             <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                 <span class="navbar-toggler-icon"></span>
