d364e0894bdd437ba2dfa6854f3d046c61794ff7
David Domingues <dadomingues2003@outlook.pt>
2024-01-18 22:25:47 +0000

    ReviewController a devolver em condiçoes

--- RMWeb/backend/modules/api/controllers/ReviewController.php
+++ RMWeb/backend/modules/api/controllers/ReviewController.php
@@ -2,8 +2,8 @@
 
 namespace backend\modules\api\controllers;
 
-use yii\web\Controller;
 use yii\rest\ActiveController;
+use common\models\Review;
 
 class ReviewController extends ActiveController
 {
@@ -13,8 +13,36 @@ class ReviewController extends ActiveController
      * Renders the index view for the module
      * @return string
      */
-    public function actionIndex()
-    {
-        return $this->render('index');
-    }
+
+     public function actions()
+     {
+         $actions = parent::actions();
+ 
+         // Disable default actions
+         unset($actions['index']);
+ 
+         return $actions;
+     }
+ 
+     public function actionIndex()
+     {
+         $reviews = Review::find()
+             ->joinWith(['user', 'restaurant'])
+             ->asArray()
+             ->all();
+ 
+         $result = [];
+         foreach ($reviews as $review) {
+             $result[] = [
+                 'id' => $review['id'],
+                 'user_name' => $review['user']['username'],
+                 'restaurant_name' => $review['restaurant']['name'],
+                 'stars' => $review['stars'],
+                 'description' => $review['description'],
+                 // Add other fields as needed
+             ];
+         }
+ 
+         return $result;
+     }
 }
